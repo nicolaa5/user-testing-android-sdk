4158a140731283e82e4a4558ee947d53dbb280cc
NiekBijman <niekbijman@gmail.com>
2020-05-12 20:40:54 +0200

    Add database infastructure using Abstract Factory for managing database

---
+++ src/main/java/com/samla/sdk/database/UserflowData.kt
@@ -0,0 +1,5 @@
+package com.samla.sdk.database
+
+class UserflowData {
+
+}
---
+++ src/main/java/com/samla/sdk/database/UserflowDatabase.kt
@@ -0,0 +1,23 @@
+package com.samla.sdk.database
+
+import android.content.Context
+
+class UserflowDatabase (context: Context) : DatabaseAccess {
+
+    override fun create(apiKey: String, data: UserflowData) {
+        TODO("not implemented")
+    }
+
+    override fun read(apiKey: String, data: UserflowData) {
+        TODO("not implemented")
+    }
+
+    override fun update(apiKey: String, data: UserflowData) {
+        TODO("not implemented")
+    }
+
+    override fun delete(apiKey: String, data: UserflowData) {
+        TODO("not implemented")
+    }
+
+}
